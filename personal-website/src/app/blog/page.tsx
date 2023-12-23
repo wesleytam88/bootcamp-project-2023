750053a1be29da81e482c052a85b4d9f677972d4
Wesley Tam <wesleytam88@gmail.com>
2023-12-22 22:19:11 -0800

    Fix blog and portfolio page async bug

--- personal-website/src/app/blog/page.tsx
+++ personal-website/src/app/blog/page.tsx
@@ -5,38 +5,36 @@ import parseTime from "@/helpers/parseTime";
 import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
 
-export default function BlogPage() {
-  async function getBlogs() {
-    await connectDB();
+async function getBlogs() {
+  await connectDB();
 
-    try {
-      // query for all blogs and sort by date
-      const blogs = await Blog.find().sort({ date: -1 }).orFail();
-      // send a response as the blogs as the message
-      return blogs;
-    } catch (err) {
-      return null;
-    }
+  try {
+    // query for all blogs and sort by date
+    const blogs = await Blog.find().sort({ date: -1 }).orFail();
+    // send a response as the blogs as the message
+    return blogs;
+  } catch (err) {
+    return null;
   }
+}
 
+export default async function BlogPage() {
+  const blogs = await getBlogs();
   return (
     <>
       <Navbar />
       <main>
         <h1 className="page-title">Blog</h1>
         <div className="blog-component">
-          {getBlogs().then(
-            (blogs) =>
-              blogs &&
-              blogs.map((blog) => (
-                <BlogComponent
-                  title={blog.title}
-                  date={parseTime(new Date(blog.date))}
-                  description={blog.description}
-                  slug={blog.slug}
-                ></BlogComponent>
-              ))
-          )}
+          {blogs &&
+            blogs.map((blog) => (
+              <BlogComponent
+                title={blog.title}
+                date={parseTime(new Date(blog.date))}
+                description={blog.description}
+                slug={blog.slug}
+              ></BlogComponent>
+            ))}
         </div>
       </main>
       <Footer />
--- personal-website/src/app/portfolio/page.tsx
+++ personal-website/src/app/portfolio/page.tsx
@@ -4,47 +4,45 @@ import ProjectComponent from "@/components/project";
 import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
 
-export default function PortfolioPage() {
-  type Project = {
-    name: string;
-    description: string;
-    image: string;
-    image_alt: string;
-    link: string;
-  };
+type Project = {
+  name: string;
+  description: string;
+  image: string;
+  image_alt: string;
+  link: string;
+};
 
-  async function getProjects() {
-    await connectDB();
+async function getProjects() {
+  await connectDB();
 
-    try {
-      // query for all blogs and sort by date
-      const blogs = await Project.find().sort({ date: -1 }).orFail();
-      // send a response as the blogs as the message
-      return blogs;
-    } catch (err) {
-      return null;
-    }
+  try {
+    // query for all blogs and sort by date
+    const projects = await Project.find().sort({ date: -1 }).orFail();
+    // send a response as the blogs as the message
+    return projects;
+  } catch (err) {
+    return null;
   }
+}
 
+export default async function PortfolioPage() {
+  const projects = await getProjects();
   return (
     <>
       <Navbar />
       <main>
         <h1 className="page-title">Portfolio</h1>
-        
-        {getProjects().then(
-          (projects) =>
-            projects &&
-            projects.map((project) => (
-              <ProjectComponent
-                name={project.name}
-                description={project.description}
-                image={project.image}
-                image_alt={project.image_alt}
-                link={project.link}
-              ></ProjectComponent>
-            ))
-        )}
+
+        {projects &&
+          projects.map((project) => (
+            <ProjectComponent
+              name={project.name}
+              description={project.description}
+              image={project.image}
+              image_alt={project.image_alt}
+              link={project.link}
+            ></ProjectComponent>
+          ))}
       </main>
       <Footer />
     </>
